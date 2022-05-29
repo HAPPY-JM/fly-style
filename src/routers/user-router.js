2a5746827cb83fb83503b9e7840beb4e5f609074
hb912 <hb912@naver.com>
2022-05-29 13:49:05 +0900

    코드리뷰

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -1,19 +1,19 @@
-import { Router } from 'express';
-import is from '@sindresorhus/is';
+import { Router } from "express";
+import is from "@sindresorhus/is";
 // 폴더에서 import하면, 자동으로 폴더의 index.js에서 가져옴
-import { loginRequired } from '../middlewares';
-import { userService } from '../services';
+import { loginRequired } from "../middlewares";
+import { userService } from "../services";
 
 const userRouter = Router();
 
 // 회원가입 api (아래는 /register이지만, 실제로는 /api/register로 요청해야 함.)
-userRouter.post('/register', async (req, res, next) => {
+userRouter.post("/register", async (req, res, next) => {
   try {
     // Content-Type: application/json 설정을 안 한 경우, 에러를 만들도록 함.
     // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
     if (is.emptyObject(req.body)) {
       throw new Error(
-        'headers의 Content-Type을 application/json으로 설정해주세요'
+        "headers의 Content-Type을 application/json으로 설정해주세요"
       );
     }
 
@@ -38,12 +38,12 @@ userRouter.post('/register', async (req, res, next) => {
 });
 
 // 로그인 api (아래는 /login 이지만, 실제로는 /api/login로 요청해야 함.)
-userRouter.post('/login', async function (req, res, next) {
+userRouter.post("/login", async function (req, res, next) {
   try {
     // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
     if (is.emptyObject(req.body)) {
       throw new Error(
-        'headers의 Content-Type을 application/json으로 설정해주세요'
+        "headers의 Content-Type을 application/json으로 설정해주세요"
       );
     }
 
@@ -51,6 +51,7 @@ userRouter.post('/login', async function (req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
+    //유저서비스
     // 로그인 진행 (로그인 성공 시 jwt 토큰을 프론트에 보내 줌)
     const userToken = await userService.getUserToken({ email, password });
 
@@ -63,7 +64,7 @@ userRouter.post('/login', async function (req, res, next) {
 
 // 전체 유저 목록을 가져옴 (배열 형태임)
 // 미들웨어로 loginRequired 를 썼음 (이로써, jwt 토큰이 없으면 사용 불가한 라우팅이 됨)
-userRouter.get('/userlist', loginRequired, async function (req, res, next) {
+userRouter.get("/userlist", loginRequired, async function (req, res, next) {
   try {
     // 전체 사용자 목록을 얻음
     const users = await userService.getUsers();
@@ -78,7 +79,7 @@ userRouter.get('/userlist', loginRequired, async function (req, res, next) {
 // 사용자 정보 수정
 // (예를 들어 /api/users/abc12345 로 요청하면 req.params.userId는 'abc12345' 문자열로 됨)
 userRouter.patch(
-  '/users/:userId',
+  "/users/:userId",
   loginRequired,
   async function (req, res, next) {
     try {
@@ -86,7 +87,7 @@ userRouter.patch(
       // 설정 안 하고 요청하면, body가 비어 있게 됨.
       if (is.emptyObject(req.body)) {
         throw new Error(
-          'headers의 Content-Type을 application/json으로 설정해주세요'
+          "headers의 Content-Type을 application/json으로 설정해주세요"
         );
       }
 
@@ -105,7 +106,7 @@ userRouter.patch(
 
       // currentPassword 없을 시, 진행 불가
       if (!currentPassword) {
-        throw new Error('정보를 변경하려면, 현재의 비밀번호가 필요합니다.');
+        throw new Error("정보를 변경하려면, 현재의 비밀번호가 필요합니다.");
       }
 
       const userInfoRequired = { userId, currentPassword };
--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,10 +1,11 @@
-import * as Api from '/api.js';
-import { validateEmail } from '/useful-functions.js';
+// import res from "/express/lib/response";
+import * as Api from "/api.js";
+import { validateEmail } from "/useful-functions.js";
 
 // 요소(element), input 혹은 상수
-const emailInput = document.querySelector('#emailInput');
-const passwordInput = document.querySelector('#passwordInput');
-const submitButton = document.querySelector('#submitButton');
+const emailInput = document.querySelector("#emailInput");
+const passwordInput = document.querySelector("#passwordInput");
+const submitButton = document.querySelector("#submitButton");
 
 addAllElements();
 addAllEvents();
@@ -14,7 +15,7 @@ async function addAllElements() {}
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
-  submitButton.addEventListener('click', handleSubmit);
+  submitButton.addEventListener("click", handleSubmit);
 }
 
 // 로그인 진행
@@ -30,7 +31,7 @@ async function handleSubmit(e) {
 
   if (!isEmailValid || !isPasswordValid) {
     return alert(
-      '비밀번호가 4글자 이상인지, 이메일 형태가 맞는지 확인해 주세요.'
+      "비밀번호가 4글자 이상인지, 이메일 형태가 맞는지 확인해 주세요."
     );
   }
 
@@ -38,19 +39,25 @@ async function handleSubmit(e) {
   try {
     const data = { email, password };
 
-    const result = await Api.post('/api/login', data);
+    const result = await Api.post("/api/login", data);
     const token = result.token;
 
     // 로그인 성공, 토큰을 세션 스토리지에 저장
     // 물론 다른 스토리지여도 됨
-    sessionStorage.setItem('token', token);
+    sessionStorage.setItem("token", token);
 
     alert(`정상적으로 로그인되었습니다.`);
 
     // 로그인 성공
-
+    //api get을 통한 userlist 불러오기는 가능한데 그냥 get userlist라우팅은 안됨(토큰 잃어버린다) 왜??
+    //get으로 불러온 데이터를 넣어서 routing 어떻게 할지 생각해보면 좋을듯..?
+    const userlist = await Api.get("/api/userlist");
+    console.log(userlist);
     // 기본 페이지로 이동
-    window.location.href = '/';
+    // 원래 코드
+    // window.location.href = "/";
+
+    //window.location.href = "/api/userlist";
   } catch (err) {
     console.error(err.stack);
     alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
