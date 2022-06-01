7c11e08b254a8200865e1f977e55748a47c29071
lcy <cymarch98@gmail.com>
2022-06-01 15:11:55 +0900

    상품스키마 순서수정
    브랜드를 상품설명 뒤로

--- src/db/schemas/product-schema.js
+++ src/db/schemas/product-schema.js
@@ -2,25 +2,30 @@ import { Schema } from "mongoose";
 
 const ProductSchema = new Schema({
     _id:objectId,//상품Id
+
     Name:{
         type:String,
         Required:true,
     },//상품이름
+
     Category:{
         type: [String],
         required:true,
     },//상품카테고리
-    Brand:{
-        type:String,
-    },//제조사
+
     Price:{
         type:Number,
         Required : true,
     },//가격
+
     Content:{
         type:String,
         Required: true,
     },//상품설명
+
+    Brand:{
+        type:String,
+    },//제조사
     
     // Img:{
 
