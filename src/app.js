20e0f28a619b70bfe679be0e57083f870d302cd5
GitSeongGit <102665966+GitSeongGit@users.noreply.github.com>
2022-06-03 05:45:08 +0900

    test 이미지 파일 삭제

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import { viewsRouter, userRouter , multerRouter} from './routers';
 import { errorHandler } from './middlewares';
 
 const app = express();
-
 // CORS 에러 방지
 app.use(cors());
 
@@ -13,10 +12,10 @@ app.use(express.json());
 
 // Content-Type: application/x-www-form-urlencoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.urlencoded({ extended: false }));
-
 // html, css, js 라우팅
 app.use(viewsRouter);
-app.use(multerRouter)
+app.use('/',multerRouter);
+//이미 파일 가져 올때 사용
 // api 라우팅
 // 아래처럼 하면, userRouter 에서 '/login' 으로 만든 것이 실제로는 앞에 /api가 붙어서
 // /api/login 으로 요청을 해야 하게 됨. 백엔드용 라우팅을 구분하기 위함임.
