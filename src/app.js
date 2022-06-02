9633b62f2f76c7ee4dd22bf33824ab21d08d99c9
hb912 <43055448+hb912@users.noreply.github.com>
2022-06-02 18:19:27 +0900

    product 라우팅 기능 정상화
    product 라우팅 기능 정상화

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import cors from "cors";
 import express from "express";
-import { viewsRouter, userRouter, orderRouter, adminRouter } from "./routers";
+import {
+  viewsRouter,
+  userRouter,
+  orderRouter,
+  adminRouter,
+  productRouter,
+} from "./routers";
 import { adminRequired, errorHandler, loginRequired } from "./middlewares";
 
 const app = express();
@@ -23,7 +29,7 @@ app.use(viewsRouter);
 app.use("/api", userRouter);
 app.use("/admins", loginRequired, adminRequired, adminRouter);
 app.use("/order", orderRouter);
-// app.use('/product', productRouter);
+app.use("/product", productRouter);
 
 // 순서 중요 (errorHandler은 다른 일반 라우팅보다 나중에 있어야 함)
 // 그래야, 에러가 났을 때 next(error) 했을 때 여기로 오게 됨
--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -2,92 +2,89 @@ import { Router } from "express";
 
 // 폴더에서 import하면, 자동으로 폴더의 index.js에서 가져옴
 import { loginRequired } from "../middlewares";
-import { adminRequired } from '../middlewares';
+import { adminRequired } from "../middlewares";
 import { productService } from "../services";
-
-
 
 const productRouter = Router();
 
-
 //상품 등록 (login 확인, admin 확인)
-productRouter.post('/add', loginRequired, adminRequired, async (req, res, next) => {
-    try{
-        {const { name, category, price, content, brand, size } = req.body;
+productRouter.post(
+  "/add",
+  /*loginRequired, adminRequired,*/ async (req, res, next) => {
+    try {
+      {
+        const { name, category, price, content, brand, size } = req.body;
 
         const newProduct = await productService.addProduct({
-            name,
-            category,
-            price,
-            content,
-            brand,
-            size
+          name,
+          category,
+          price,
+          content,
+          brand,
+          size,
         });
 
         res.redirect(`/product/${newProduct._id}`);
-        }
-    }catch(err){
-        next(err);
+      }
+    } catch (err) {
+      next(err);
     }
-}); 
-
-
+  }
+);
 
 //상품 목록
-productRouter.get('/', async (req, res) => {
-    const productList = await productService.productList();
+productRouter.get("/", async (req, res) => {
+  const productList = await productService.productList();
 
-    res.json(productList);
-}); 
-
-
+  res.json(productList);
+});
 
 //상품 수정 (login 확인, admin 확인)
-productRouter.patch('/edit/:id', loginRequired, adminRequired, async(req, res) => {
+productRouter.patch(
+  "/edit/:id",
+  loginRequired,
+  adminRequired,
+  async (req, res) => {
     const productId = req.params.id;
 
     const { name, category, price, content, brand, size } = req.body;
 
     const updateData = {
-        ...(name && {name}),
-        ...(category && {category}),
-        ...(price && {price}),
-        ...(content && {content}),
-        ...(brand && {brand}),
-        ...(size && {size}),
+      ...(name && { name }),
+      ...(category && { category }),
+      ...(price && { price }),
+      ...(content && { content }),
+      ...(brand && { brand }),
+      ...(size && { size }),
     };
 
     await productService.editProduct(productId, updateData);
 
     res.redirect(`/product/${productId}`);
-}); 
-
+  }
+);
 
 //상품 삭제 (login 확인, admin 확인)
-productRouter.delete('/:id', loginRequired, adminRequired, async(req, res) => {
-    const productId = req.params.id;
+productRouter.delete("/:id", loginRequired, adminRequired, async (req, res) => {
+  const productId = req.params.id;
 
-    await productService.deleteProduct(productId);
-    // res.send(`상품을 삭제했습니다.`);
-    res.redirect('/');
+  await productService.deleteProduct(productId);
+  // res.send(`상품을 삭제했습니다.`);
+  res.redirect("/");
 });
 
 //카테고리별 상품
-productRouter.get('/:category', async(req, res) => {
-    const category = req.params.category;
-    const categoryProduct = await productService.findByCategory(category);
-    res.json(categoryProduct);
+productRouter.get("/:category", async (req, res) => {
+  const category = req.params.category;
+  const categoryProduct = await productService.findByCategory(category);
+  res.json(categoryProduct);
 });
-
 
 //상품 상세 보기
-productRouter.get('/:id', async(req, res) => {
-    const productId = req.params.id;
-    const productData = await productService.viewProductData(productId);
-    res.json(productData);
+productRouter.get("/:id", async (req, res) => {
+  const productId = req.params.id;
+  const productData = await productService.viewProductData(productId);
+  res.json(productData);
 });
 
-
-
 export { productRouter };
-
