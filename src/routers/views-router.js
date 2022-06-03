ef7bb832e10a5dad95d7590fac41d5dd13c7a5ac
정민희 <dev.alsgml1640@gmail.com>
2022-06-03 17:51:19 +0900

    feat : cart view 생성
    장바구니 뷰에서
    일단 헤더와 푸터만 넣은 상태

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -10,7 +10,12 @@ viewsRouter.use("/", serveStatic("home"));
 viewsRouter.use("/register", serveStatic("register"));
 viewsRouter.use("/login", serveStatic("login"));
 
+viewsRouter.use("/product-detail", serveStatic("/product-detail"));
+
 viewsRouter.use("/product-list", serveStatic("product-list"));
+viewsRouter.use("/cart", serveStatic("cart"));
+
+
 //viewsRouter.use("/dkssudgktpdyadmin", serveStatic("login"));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
