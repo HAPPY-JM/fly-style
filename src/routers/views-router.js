f6a2b1b7c523a08a1847de346ea138bf2303a730
정민희 <dev.alsgml1640@gmail.com>
2022-06-03 22:03:23 +0900

    [feat] 상품 목록 최종 정리 및 카트 간략 구현

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -10,6 +10,7 @@ viewsRouter.use("/", serveStatic("home"));
 viewsRouter.use("/register", serveStatic("register"));
 viewsRouter.use("/login", serveStatic("login"));
 viewsRouter.use("/product-list", serveStatic("product-list"));
+viewsRouter.use("/cart", serveStatic("cart"));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use("/", serveStatic(""));
