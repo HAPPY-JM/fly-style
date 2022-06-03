c107080ab2ca42953bcf6aacf1a40dd1d5a7e6f2
정민희 <dev.alsgml1640@gmail.com>
2022-06-03 21:54:24 +0900

    [feat] 상품목록 리스트 구현 및 카트 구현
    상품목록 리스트 및 카트 간략 구현

--- src/views/product-list/product-list.js
+++ src/views/product-list/product-list.js
@@ -1,46 +1,40 @@
 const productSection = document.querySelector('.section');
 
+// mock data 생성하여(./product.json) 가져온 데이터를 화면에 뿌려준다. 
+const url = './products.json';
+const response = await fetch(url);
+const data = await response.json();
 
+const productsObj = data.products;
 
+// 상품 목록에 넣을 데이터 변수
 let productInnerData = "";
 
-const productDataDiv = `
-<div class="card product-item">
-<a href="#">
-    <div class="card-image">
-        <figure class="image is-square">
-            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-        </figure>
-    </div>
-    <div class="card-content">
-        <div class="media">
-            <div class="media-content">
-                <p class="title is-4">Nike</p>
-                <p class="subtitle is-6">Jordan 1 Retro High OG Black Mocha</p>
-                <p class="title is-5">12000원</p>
+function getProductsListData(e){
+    productInnerData +=  `
+    <div class="card product-item">
+    <a href="#">
+        <div class="card-image">
+            <figure class="image is-square">
+                <img src="${e.imgSrc}" alt="Placeholder image">
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="media">
+                <div class="media-content">
+                    <p class="title is-5">${e.name}</p>
+                    <p class="subtitle is-7">${e.content}</p>
+                    <p class="title is-6">${e.price}</p>
+                </div>
             </div>
         </div>
+    </a>
     </div>
-</a>
-</div>
-`;
-
-for(let i = 0 ; i < 10 ; i++){
-    productInnerData += productDataDiv;
+    `
 }
 
+productsObj.map(e => getProductsListData(e));
+
 productSection.innerHTML = productInnerData;
-
-
-// mock data 생성하여(./product.json) 가져온 데이터를 화면에 뿌려준다. 
-const productJson = fetch('./product.json')
-.then((res) => res.json())
-.then((data)=> data)
-.then((result) =>result.products);
-
-
-//console.log(JSON.parse(productJson));
-console.log(productJson);
-
 
     
