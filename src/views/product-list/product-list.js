a77ea61dbe0dff9115e1d01cf6789dfbf8f5d501
정민희 <dev.alsgml1640@gmail.com>
2022-06-03 14:16:53 +0900

    [feat] 상품 목록 json과 js 연결
    목데이터로 만들어 놓은 상품목록 json 파일을 일단 먼저 js 과 연결하여
    콘솔에 잘 찍히는지 확인!

--- src/views/product-list/product-list.js
+++ src/views/product-list/product-list.js
@@ -1,6 +1,13 @@
 const productSection = document.querySelector('.section');
 
+// mock data 생성하여(./product.json) 가져온 데이터를 화면에 뿌려준다. 
+const url = './products.json';
+const response = await fetch(url);
+const data = await response.json();
 
+const productsObj = data.products;
+//console.log(JSON.parse(productJson));
+console.log(productsObj);
 
 let productInnerData = "";
 
@@ -32,15 +39,7 @@ for(let i = 0 ; i < 10 ; i++){
 productSection.innerHTML = productInnerData;
 
 
-// mock data 생성하여(./product.json) 가져온 데이터를 화면에 뿌려준다. 
-const productJson = fetch('./product.json')
-.then((res) => res.json())
-.then((data)=> data)
-.then((result) =>result.products);
 
-
-//console.log(JSON.parse(productJson));
-console.log(productJson);
 
 
     
