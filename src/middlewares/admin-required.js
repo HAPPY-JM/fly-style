aa93c77870dc1a87a451a2254feb0b98b17eab4d
hb912 <43055448+hb912@users.noreply.github.com>
2022-06-04 19:24:36 +0900

    코드리뷰 후 코드 수정
    코드리뷰 후 주석 및 코드 수정

--- src/middlewares/admin-required.js
+++ src/middlewares/admin-required.js
@@ -1,6 +1,5 @@
 //관리자만 접근할 수 있는 api 일반 유저가 접근했을때 걸러주는 미들웨어
 function adminRequired(req, res, next) {
-  console.log(req.currentUserRole);
   if (req.currentUserRole !== "admin") {
     res.status(403).json({
       result: "forbidden-approach",
--- src/routers/admin-router.js
+++ src/routers/admin-router.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-// 폴더에서 import하면, 자동으로 폴더의 index.js에서 가져옴
 import { userService } from "../services";
 
 const adminRouter = Router();
