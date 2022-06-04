0e31ec4ad59577f7851b687d00a57f6199526c28
lcy <cymarch98@gmail.com>
2022-06-04 17:56:20 +0900

    주석 수정
    변수만 보고 짐작할 수 있는 부분의 주석 삭제

--- src/db/schemas/product-schema.js
+++ src/db/schemas/product-schema.js
@@ -2,31 +2,30 @@ import { Schema } from "mongoose";
 import { CategoryModel } from "../models/category-model";
 
 const ProductSchema = new Schema({
-  // _id:objectId,//상품Id
 
   name: {
     type: String,
     required: true,
-  }, //상품이름
+  }, 
 
   category: {
     type: Schema.Types.ObjectId,
     ref : 'category',
-  }, //상품카테고리
+  }, 
 
   price: {
     type: Number,
     required: true,
-  }, //가격
+  }, 
 
   content: {
     type: String,
     required: true,
-  }, //상품설명
+  }, //상품 설명
 
   brand: {
     type: String,
-  }, //제조사
+  }, 
 
   // Img:{
 
