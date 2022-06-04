42a21e28be45847f025636e9845cc1b0ccbd5afd
lcy <cymarch98@gmail.com>
2022-06-04 18:05:50 +0900

    미들웨어 import 수정
    두 줄로 쓰여있는 것 한 줄로 수정

--- src/routers/category-router.js
+++ src/routers/category-router.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
 
 // 폴더에서 import하면, 자동으로 폴더의 index.js에서 가져옴
-import { loginRequired } from "../middlewares";
-import { adminRequired } from '../middlewares';
+import { loginRequired, adminRequired } from "../middlewares";
 import { categoryService } from "../services";
 
 const categoryRouter = Router();
