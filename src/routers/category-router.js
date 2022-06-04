9e22494c313503885a230f2124cfbdb9fd53fb25
hb912 <43055448+hb912@users.noreply.github.com>
2022-06-04 20:54:02 +0900

    프리티어 configuration

--- src/routers/category-router.js
+++ src/routers/category-router.js
@@ -5,47 +5,57 @@ import { categoryService } from "../services";
 const categoryRouter = Router();
 
 //카테고리 등록 (login 확인, admin 확인)
-categoryRouter.post("/",loginRequired,adminRequired,async (req, res, next) => {
+categoryRouter.post(
+  "/",
+  loginRequired,
+  adminRequired,
+  async (req, res, next) => {
     try {
-        const { name } = req.body;
+      const { name } = req.body;
 
-        const newCategory = await categoryService.addCategory({name});
+      const newCategory = await categoryService.addCategory({ name });
 
-        res.json(newCategory);
+      res.json(newCategory);
     } catch (err) {
-        next(err);
+      next(err);
     }
-});
+  }
+);
 
 //카테고리 목록
 categoryRouter.get("/", async (req, res) => {
-    const categoryList = await categoryService.categoryList();
+  const categoryList = await categoryService.categoryList();
 
-    res.json(categoryList);
+  res.json(categoryList);
 });
 
 //카테고리 수정 (login 확인, admin 확인)
 categoryRouter.patch("/:id", loginRequired, adminRequired, async (req, res) => {
-    const categoryId = req.params.id;
+  const categoryId = req.params.id;
 
-    const { name } = req.body;
+  const { name } = req.body;
 
-    const updateData = {
-        ...(name && { name }),
-    };
+  const updateData = {
+    ...(name && { name }),
+  };
 
-    await categoryService.editCategory(categoryId, updateData);
+  await categoryService.editCategory(categoryId, updateData);
 
-    res.redirect("/category");
+  res.redirect("/category");
 });
 
 //카테고리 삭제 (login 확인, admin 확인)
-categoryRouter.delete("/:id",loginRequired,adminRequired,async (req, res) => {
+categoryRouter.delete(
+  "/:id",
+  loginRequired,
+  adminRequired,
+  async (req, res) => {
     const categoryId = req.params.id;
 
     await categoryService.deleteCategory(categoryId);
     // res.send(`카테고리를 삭제했습니다.`);
     res.redirect("/");
-});
+  }
+);
 
 export { categoryRouter };
