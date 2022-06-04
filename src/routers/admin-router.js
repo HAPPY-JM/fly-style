3bdd5010619d751092436ea1409991a5e29ba82b
hb912 <43055448+hb912@users.noreply.github.com>
2022-06-05 02:24:02 +0900

    order 라우팅 수정
    주문 라우팅 중 admin 전용 기능 admin 라우터로 수정<br>
    주문 추가부분 phonenum 등 추가

--- src/routers/admin-router.js
+++ src/routers/admin-router.js
@@ -15,4 +15,41 @@ adminRouter.get("/userList", async function (req, res, next) {
   }
 });
 
+//모든 주문 목록 가져오기
+adminRouter.get("/order/lists", async function (req, res, next) {
+  try {
+    const Orderlists = await orderService.orderLists();
+    res.status(201).json(Orderlists);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//주문상태변경
+adminRouter.patch("/orderStat/:orderId", async function (req, res, next) {
+  try {
+    const _id = mongoose.Types.ObjectId(req.params.orderId);
+    const { orderStatus } = req.body;
+    const result = await orderService.orderUpdate(_id, orderStatus);
+
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//주문 삭제
+adminRouter.delete("/order/:orderId", async function (req, res, next) {
+  const _id = mongoose.Types.ObjectId(req.params.orderId);
+
+  try {
+    const result = await orderService.orderDelete(_id);
+
+    //주문삭제 결과 전송
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
 export { adminRouter };
