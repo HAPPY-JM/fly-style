481902975e1ce589b936256c2a2c486f950807e8
lcy <cymarch98@gmail.com>
2022-06-04 20:07:19 +0900

    상품/카테고리 라우터 수정
    - 미들웨어 import 두 줄을 한 줄로 합치기
    - 코드 들여쓰기,줄바꿈 등 수정
    - 경로에서 add/edit등 삭제
    - try문 안에 중괄호가 한 번 더 있는 것 삭제

--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -1,18 +1,15 @@
 import { Router } from "express";
 
 // 폴더에서 import하면, 자동으로 폴더의 index.js에서 가져옴
-import { loginRequired } from "../middlewares";
-
-import { adminRequired } from '../middlewares';
+import { loginRequired, adminRequired } from "../middlewares";
 import { productService, categoryService } from "../services";
 
 
 const productRouter = Router();
 
 //상품 등록 (login 확인, admin 확인)
-productRouter.post("/add",/*loginRequired, adminRequired,*/ async (req, res, next) => {
+productRouter.post("/", loginRequired, adminRequired, async (req, res, next) => {
     try {
-        {
         const { name, category, price, content, brand, size } = req.body;
 
         const newProduct = await productService.addProduct({
@@ -25,12 +22,10 @@ productRouter.post("/add",/*loginRequired, adminRequired,*/ async (req, res, nex
         });
 
         res.redirect(`/product/${newProduct._id}`);
-        }
     } catch (err) {
         next(err);
     }
-    }
-);
+});
 
 //상품 목록
 productRouter.get("/", async (req, res) => {
@@ -40,7 +35,7 @@ productRouter.get("/", async (req, res) => {
 });
 
 //상품 수정 (login 확인, admin 확인)
-productRouter.patch("/edit/:id",loginRequired,adminRequired,async (req, res) => {
+productRouter.patch("/:id", loginRequired, adminRequired, async (req, res) => {
     const productId = req.params.id;
 
     const { name, category, price, content, brand, size } = req.body;
@@ -79,7 +74,7 @@ productRouter.delete("/:id", loginRequired, adminRequired, async (req, res) => {
 
 
 //상품 상세 보기
-productRouter.get("/detail/:id", async (req, res) => {
+productRouter.get("/:id", async (req, res) => {
     const productId = req.params.id;
     console.log(productId);
     const productData = await productService.viewProductData(productId);
