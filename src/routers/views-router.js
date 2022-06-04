3891c9a7ff9e1ebcc4e36f88876975984be5e665
hb912 <43055448+hb912@users.noreply.github.com>
2022-06-05 02:24:53 +0900

    뷰라우팅 수정
    products,product 라우팅 수정 및 오더 뷰 라우팅 추가

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -10,11 +10,12 @@ viewsRouter.use("/", serveStatic("home"));
 viewsRouter.use("/register", serveStatic("register"));
 viewsRouter.use("/login", serveStatic("login"));
 
-viewsRouter.use("/product/detail", serveStatic("/product-detail"));
+viewsRouter.use("/product", serveStatic("/product-detail"));
 viewsRouter.use("/dkssudgktpdyadmin", serveStatic("login"));
 
-viewsRouter.use("/product/list", serveStatic("product-list"));
+viewsRouter.use("/products", serveStatic("product-list"));
 viewsRouter.use("/cart", serveStatic("cart"));
+viewsRouter.use("/order", serveStatic("order"));
 
 //viewsRouter.use("/dkssudgktpdyadmin", serveStatic("login"));
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
