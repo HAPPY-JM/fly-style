dfb14c226f2864af531a2e834684c71de783901c
lcy <cymarch98@gmail.com>
2022-06-05 16:29:01 +0900

    app.js 카테고리라우터 경로수정
    /category -> /api/category

--- src/app.js
+++ src/app.js
@@ -31,10 +31,9 @@ app.use("/api", userRouter);
 
 app.use("/api/admins", loginRequired, adminRequired, adminRouter);
 app.use("/api/order", loginRequired, orderRouter);
-// app.use("/api/product", productRouter);
 
 app.use("/api/product", productRouter);
-app.use("/category", categoryRouter);
+app.use("/api/category", categoryRouter);
 
 // 순서 중요 (errorHandler은 다른 일반 라우팅보다 나중에 있어야 함)
 // 그래야, 에러가 났을 때 next(error) 했을 때 여기로 오게 됨
