5e40a770a5915d50c5ce9108a5f00d05b6bdaf84
정민희 <dev.alsgml1640@gmail.com>
2022-06-06 21:48:11 +0900

    Refactor : 함수명 변경

--- src/views/product-list/product-list.js
+++ src/views/product-list/product-list.js
@@ -1,59 +1,59 @@
 import * as Api from "/api.js";
+import dom from "/dom.js";
 
-const productSection = document.querySelector(".section");
+console.log(Api);
+console.log('----');
+console.log(Api.get());
+console.log('----');
+console.log(Api.post());
 
-// mock data 생성하여(./product.json) 가져온 데이터를 화면에 뿌려준다.
-// const url = "./products.json";
-const data = await Api.get("/api/product");
-// const response = await fetch(url);
-// const data = await response.json();
-console.log(data);
-
-const productsObj = data.products;
+// 상품 리스트 섹션
+const productSection = dom('.section');
+// mock data 생성하여(./product.json) 가져온 데이터를 화면에 뿌려준다. 
+const productsUrl = './products.json';
+const productsResponse = await fetch(productsUrl);
+const productsData = await productsResponse.json();
+const productsObj = productsData.products;
 console.log(productsObj);
 
 // 상품 목록에 넣을 데이터 변수
 let productInnerData = "";
 
-// get API로 데이터 불러옴
-// const data = {_id, name }
+// 카테고리 섹션 - 메뉴 리스트
+const categorySection = dom('.category-list .menu-list');
 
-// '/product-detail/:_id'
-// /<a href="/product-detail/${_id}">
-// 상세페이지
-// const id = _id
-// Api.get('/Api/product-detail/', id)
 
-/* 
-1. 각자 목록에 아이디  설정
-2. a href=''
+// 카테고리 mock data 가져오기
+const categoryUrl = './category.json';
+const categoryResponse = await fetch(categoryUrl);
+const categoryData = await categoryResponse.json();
+const categoryObj = categoryData.category;
+// console.log(categoryObj);
 
-const data = await Api.get(`/api/email/${sessionStorage.getItem('email')}`);
-*/
-
-function getProductsListData(e) {
-  productInnerData += `
-    <div class="card product-item" id="productNum${e.id}">
-    <a href="/product?id=${e._id}">
+function addProductsListData(productData){
+    //"/product-detail?id="
+    productInnerData +=  `
+    <div class="card product-item" id="productNum${productData._id}">
+    <a href="/product?_id=${productData._id}">
         <div class="card-image">
             <figure class="image is-square">
-                <img src="${e.imgSrc}" alt="Placeholder image">
+                <img src="${productData.imgSrc}" alt="Placeholder image">
             </figure> 
         </div>
         <div class="card-content">
             <div class="media">
                 <div class="media-content">
-                    <p class="title is-5">${e.name}</p>
-                    <p class="subtitle is-7">${e.content}</p>
-                    <p class="title is-6">${e.price}</p>
+                    <p class="title is-5">${productData.name}</p>
+                    <p class="subtitle is-7">${productData.content}</p>
+                    <p class="title is-6">${productData.price}</p>
                 </div>
             </div>
         </div>
     </a>
     </div>
-    `;
+    `
 }
 
-data.map((e) => getProductsListData(e));
+productsObj.map(productData => addProductsListData(productData));
 
 productSection.innerHTML = productInnerData;
