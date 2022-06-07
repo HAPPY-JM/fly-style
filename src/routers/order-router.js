3fc03a86f19735bebaa197b86f70b7ddfc81817e
hb912 <43055448+hb912@users.noreply.github.com>
2022-06-07 13:05:53 +0900

    오더라우팅수정
    오더라우팅 중 관리자 관련 기능 수정

--- src/routers/order-router.js
+++ src/routers/order-router.js
@@ -91,41 +91,4 @@ orderRouter.get("/detail/:orderId", async function (req, res, next) {
   }
 });
 
-//모든 주문 목록 가져오기
-orderRouter.get("/lists", async function (req, res, next) {
-  try {
-    const Orderlists = await orderService.orderLists();
-    res.status(201).json(Orderlists);
-  } catch (error) {
-    next(error);
-  }
-});
-
-//주문상태변경
-orderRouter.patch("/orderStat/:orderId", async function (req, res, next) {
-  try {
-    const _id = mongoose.Types.ObjectId(req.params.orderId);
-    const { orderStatus } = req.body;
-    const result = await orderService.orderUpdate(_id, orderStatus);
-
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-//주문 삭제
-orderRouter.delete("/:orderId", async function (req, res, next) {
-  const _id = mongoose.Types.ObjectId(req.params.orderId);
-
-  try {
-    const result = await orderService.orderDelete(_id);
-
-    //주문삭제 결과 전송
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
 export { orderRouter };
