98fdad17eabd477de9038e6118231cd5b0b87ae0
hb912 <43055448+hb912@users.noreply.github.com>
2022-06-07 13:10:32 +0900

    카테고리 명으로 카테고리 id 조회
    카테고리 명으로 카테고리 id를 조회하고 이 id로 상품 목록을 조회하는 기능 생성

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -3,67 +3,63 @@ import mongoose from "mongoose";
 
 //프로덕트라우터에서 사용
 class ProductService {
-    // 본 파일의 맨 아래에서, new ProductService(productModel) 하면, 이 함수의 인자로 전달됨
-    constructor(productModel) {
-        this.productModel = productModel;
+  // 본 파일의 맨 아래에서, new ProductService(productModel) 하면, 이 함수의 인자로 전달됨
+  constructor(productModel) {
+    this.productModel = productModel;
+  }
+
+  // 상품등록
+  async addProduct(productInfo) {
+    // 객체 destructuring
+    const { name, category, price, content, brand, size } = productInfo;
+
+    // 상품명 중복 확인
+    const nameProduct = await this.productModel.findByName(name);
+    if (nameProduct) {
+      throw new Error(
+        "이 상품명은 이미 존재합니다. 다른 상품명을 입력해 주세요."
+      );
     }
-    
-    // 상품등록
-    async addProduct(productInfo) {
-      // 객체 destructuring
-        const { name, category, price, content, brand, size } = productInfo;
-    
-      // 상품명 중복 확인
-        const nameProduct = await this.productModel.findByName(name);
-        if (nameProduct) {
-            throw new Error(
-                "이 상품명은 이미 존재합니다. 다른 상품명을 입력해 주세요."
-            );
-        }
-      // 상품명 중복은 이제 아니므로, 상품등록을 진행함
+    // 상품명 중복은 이제 아니므로, 상품등록을 진행함
 
-        const newProductInfo = { name, category, price, content, brand, size };
+    const newProductInfo = { name, category, price, content, brand, size };
 
-      // db에 저장
-        const createdNewProduct = await this.productModel.create(newProductInfo);
+    // db에 저장
+    const createdNewProduct = await this.productModel.create(newProductInfo);
 
-        return createdNewProduct;
-        
-    }
+    return createdNewProduct;
+  }
 
-    //상품 목록
-    async productList(){
-        const products = await this.productModel.findAll();
-        return products;
-    }
+  //상품 목록
+  async productList() {
+    const products = await this.productModel.findAll();
+    return products;
+  }
 
+  //상품 수정
+  async editProduct(productId, update) {
+    const editProduct = await this.productModel.editProduct(productId, update);
+    return editProduct;
+  }
 
-    //상품 수정
-    async editProduct( productId, update ) {
-        const editProduct = await this.productModel.editProduct(productId, update);
-        return editProduct;
-    }
+  //상품 삭제
+  async deleteProduct(productId) {
+    const deleteProduct = await this.productModel.deleteProduct(productId);
+    return deleteProduct;
+  }
 
-    //상품 삭제
-    async deleteProduct( productId ){
-        const deleteProduct = await this.productModel.deleteProduct(productId);
-        return deleteProduct;
-    }
+  //카테고리로 상품 찾기
+  async findByCategory(category) {
+    const findProduct = await this.productModel.findByCategory(category);
+    return findProduct;
+  }
 
-    // //카테고리로 상품 찾기
-    // async findByCategory( category ){
-    //     const findProduct = await this.productModel.findByCategory(category);
-    //     return findProduct;
-    // }
-
-    //상품 상세
-    async viewProductData( productId ){
-        const product = await this.productModel.findById( productId );
-        return product;
-    }
-
+  //상품 상세
+  async viewProductData(productId) {
+    const product = await this.productModel.findById(productId);
+    return product;
+  }
 }
-
 
 const productService = new ProductService(productModel);
 
