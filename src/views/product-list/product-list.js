56f58e6da7d7ac2a2da97ee0ab367cc64c4fa754
lcy <cymarch98@gmail.com>
2022-06-08 03:15:02 +0900

    페이지네이션 수정(product-list.html/js , api.js)

--- src/views/product-list/product-list.js
+++ src/views/product-list/product-list.js
@@ -1,5 +1,8 @@
-import * as Api from "/api.js";
-import { $ } from "/utils.js";
+import * as Api from "../api.js";
+import { $ } from "../utils.js";
+const URLSearch = new URLSearchParams(location.search);
+const page = Number(URLSearch.get("page")) || 1;
+
 
 //localhost:5000/products/?category="slfjsalkfjalsfjl"
 //const URLSearch = new URLSearchParams(location.search);
@@ -15,17 +18,15 @@ import { $ } from "/utils.js";
 // const response = await fetch(url);
 // const data = await response.json();
 // api에서 상품리스트 데이터와 카테고리 데이터 받아오기
-const productData = await Api.get("/api/product");
+const productData = await Api.get(`/api/product?page=${page}`);
 const categoryData = await Api.get("/api/category");
-
+console.log(productData);
 // 상품 리스트 섹션
 const productSection = $(".section");
 // 카테고리 섹션 - 메뉴 리스트
 const categorySection = $('.header-category-list');
-
 //페이지네이션
-const paginationClass = $('.pagination');
-
+const paginationClass = $(".pagination");
 
 
 // 상품 목록에 넣을 데이터 변수
@@ -41,19 +42,19 @@ function addCategoryListData(categoryData) {
 function addProductsListData(productData) {
   //"/product-detail?id="
   productInnerData += `
-    <div class="card product-item" id="${productData.productsPerPage._id}">
-    <a href="/product?_id=${productData.productsPerPage._id}">
+    <div class="card product-item" id="${productData._id}">
+    <a href="/product?_id=${productData._id}">
         <div class="card-image">
             <figure class="image is-square">
-                <img src="${productData.productsPerPage.imgSrc}" alt="Placeholder image">
+                <img src="${productData.imgSrc}" alt="Placeholder image">
             </figure> 
         </div>
         <div class="card-content">
             <div class="media">
                 <div class="media-content">
-                    <p class="title is-5">${productData.productsPerPage.name}</p>
-                    <p class="subtitle is-7">${productData.productsPerPage.content}</p>
-                    <p class="title is-6">${productData.productsPerPage.price}</p>
+                    <p class="title is-5">${productData.name}</p>
+                    <p class="subtitle is-7">${productData.content}</p>
+                    <p class="title is-6">${productData.price}</p>
                 </div>
             </div>
         </div>
@@ -62,17 +63,27 @@ function addProductsListData(productData) {
     `;
 }
 
-function pagination(productData){
-    for(let i = 1; i <= productData.totalPage; i++){
-        document.write("<td>");
-        document.write("<a href=" + `/api/product?page=${i}&perPage=${productData.perPage}` + ">" + i + "</a>");
-        document.write("</td>");
-    }
-}
 
-productData.map((productData) => addProductsListData(productData));
+function pagination(productData) {
+    let paginationEl = ``;
+    for (let i = 1; i <= productData.totalPage; i++) {
+      paginationEl += `
+          <td>
+              <a href="/product?page=${i}">
+                  ${i} 
+              </a>
+          </td>
+        `;
+    }
+    console.log(paginationEl);
+    console.log(paginationClass);
+    return paginationEl;
+  }
+
+productData.productsPerPage.map((productData) => addProductsListData(productData));
 categoryData.map((categoryData) => addCategoryListData(categoryData));
 
 productSection.innerHTML = productInnerData;
 categorySection.innerHTML = categoryInnerData;
 paginationClass.innerHTML = pagination(productData);
+
