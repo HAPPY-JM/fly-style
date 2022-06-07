55375165b599adf36fcd16597f7bff019be7f4cd
hb912 <43055448+hb912@users.noreply.github.com>
2022-06-07 14:34:48 +0900

    merge하면서 생긴 오류 수정

--- src/views/product-list/product-list.js
+++ src/views/product-list/product-list.js
@@ -1,5 +1,5 @@
 import * as Api from "/api.js";
-import dom from "/dom.js";
+import { $ } from "/utils.js";
 
 //localhost:5000/products/?category="slfjsalkfjalsfjl"
 //const URLSearch = new URLSearchParams(location.search);
@@ -10,34 +10,32 @@ import dom from "/dom.js";
 // mock data 생성하여(./product.json) 가져온 데이터를 화면에 뿌려준다.
 // const url = "./products.json";
 //특정 카테고리에 어떤 상품 리스트들이있는지 이걸 배열으로 받아와서 뿌려줘야겠죠?
-const data = await Api.get("/api/product");
 
 // GET /api/product/:id
 // const response = await fetch(url);
 // const data = await response.json();
-// api에서 상품리스트 데이터와 카테고리 데이터 받아오기 
-const productData =await Api.get('/api/product');
-const categoryData = await Api.get('/category');
+// api에서 상품리스트 데이터와 카테고리 데이터 받아오기
+const productData = await Api.get("/api/product");
+const categoryData = await Api.get("/api/category");
 
 // 상품 리스트 섹션
-const productSection = dom('.section');
+const productSection = $(".section");
 // 카테고리 섹션 - 메뉴 리스트
-const categorySection = dom('.header-category-list');
-
+const categorySection = $(".header-category-list");
 
 // 상품 목록에 넣을 데이터 변수
 let productInnerData = "";
-// 카테고리목록에 넣을 데이터 변수 
+// 카테고리목록에 넣을 데이터 변수
 let categoryInnerData = "";
 
 // 카테고리 넣을 함수 구현
-function addCategoryListData(categoryData){
-    categoryInnerData += `<li><a>${categoryData.name}</a></li>`
+function addCategoryListData(categoryData) {
+  categoryInnerData += `<li><a>${categoryData.name}</a></li>`;
 }
 // 상품 넣을 함수 구현
-function addProductsListData(productData){
-    //"/product-detail?id="
-    productInnerData +=  `
+function addProductsListData(productData) {
+  //"/product-detail?id="
+  productInnerData += `
     <div class="card product-item" id="${productData._id}">
     <a href="/product?_id=${productData._id}">
         <div class="card-image">
@@ -56,7 +54,7 @@ function addProductsListData(productData){
         </div>
     </a>
     </div>
-    `
+    `;
 }
 
 productData.map((productData) => addProductsListData(productData));
