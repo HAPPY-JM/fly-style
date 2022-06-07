9933882e77b009ba3248d2f9867e727a7645a505
jeongmin SEO <all.the.lights.on.me@gmail.com>
2022-06-07 22:03:55 +0900

    Edit: admin page, list 수정
    상품 관리 버튼을 추가하였습니다.

---
+++ src/views/admin-list/admin-list.js
@@ -0,0 +1,12 @@
+// import * as Api from "/api.js";
+import header from "/header.js";
+import { $ } from "/utils.js";
+
+// 요소(element), input 혹은 상수
+const headerParent = $(".hero");
+addAllElements();
+
+// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+async function addAllElements() {
+  header(headerParent);
+}
