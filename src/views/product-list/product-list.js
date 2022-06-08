3b5c29adfd0f2b04746a9d345c49b60903540395
정민희 <dev.alsgml1640@gmail.com>
2022-06-08 13:30:27 +0900

    update 상품리스트 헤더 컴포넌트 적용

--- src/views/product-list/product-list.js
+++ src/views/product-list/product-list.js
@@ -1,5 +1,17 @@
 import * as Api from "/api.js";
+
+import header from "/header.js";
 import { $ } from "/utils.js";
+
+// 요소(element), input 혹은 상수
+const headerParent = $("body");
+addAllElements();
+
+// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+async function addAllElements() {
+  header(headerParent);
+}
+
 
 //localhost:5000/products/?category="slfjsalkfjalsfjl"
 //const URLSearch = new URLSearchParams(location.search);
