cfd37af2096ef0662e3606ca6bb69ded7671c068
jeongmin SEO <all.the.lights.on.me@gmail.com>
2022-06-08 22:55:11 +0900

    Fix: modal icon 추가
    제곧내

--- src/views/register/register.js
+++ src/views/register/register.js
@@ -1,4 +1,5 @@
 import * as Api from '/api.js';
+import header from "/header.js";
 import { validateEmail } from '/useful-functions.js';
 
 // 요소(element), input 혹은 상수
@@ -7,6 +8,7 @@ const emailInput = document.querySelector('#emailInput');
 const passwordInput = document.querySelector('#passwordInput');
 const passwordConfirmInput = document.querySelector('#passwordConfirmInput');
 const submitButton = document.querySelector('#submitButton');
+const headerParent = $(".hero");
 
 addAllElements();
 addAllEvents();
@@ -35,45 +37,56 @@ async function handleSubmit(e) {
 	const isPasswordSame = password === passwordConfirm;
 
 	if (!isFullNameValid) {
-		return swal('이름은 2글자 이상 입력해주세요.');
+		return swal({
+			icon: "error",
+			text: '이름은 2글자 이상 입력해주세요.'
+		});
 	}
 
   if (!isPasswordValid) {
-		return swal('비밀번호는 4글자 이상으로 설정해주세요.');
+		return swal({
+			icon: "error",
+			text: '비밀번호는 4글자 이상으로 설정해주세요.'
+		});
 	}
 
 	if (!isEmailValid) {
-		return swal('이메일 형식이 맞지 않아요.');
+		return swal({
+			icon: "error",
+			text: '이메일 형식이 맞지 않아요.'
+		});
 	}
 
 	if (!isPasswordSame) {
-		return swal('비밀번호가 일치하지 않아요.');
+		return swal({
+			icon: "error",
+			text: '비밀번호가 일치하지 않아요.'
+		});
 	}
 
   // 회원가입 api 요청
-	try {
-		const data = { fullName, email, password };
-		const correctEmail = localStorage.getItem('mail');
-		if (correctEmail === emailInput.value) {
-			await Api.post('/api/register', data);
-			const login = { email, password };
-			const result = await Api.post('/api/login', login);
-			const token = result.token;
-			swal({
-				text: '가입이 완료되었어요',
-			}).then(() => {
-				localStorage.setItem('token', token);
-				window.location.href = '/';
-			});
-		}
+  try {
+    const data = { fullName, email, password };
+
+    await Api.post('/api/register', data);
+
+    swal({
+			icon: "success",
+			text: '정상적으로 회원가입되었습니다.'
+		});
 
     // 로그인 페이지 이동
     window.location.href = '/login';
   } catch (err) {
     console.error(err.stack);
-    swal({
-			text: err.message,
+    alert({
+			icon: "error",
+			text: `문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`
 		});
   }
 }
 
+
+async function addAllElements() {
+  header(headerParent);
+}
