4728919221289b3fe040d86e52ff1d86f26894de
정민희 <dev.alsgml1640@gmail.com>
2022-06-09 00:50:21 +0900

    update : 주문목록 리스트
    뷰 구현 및 삭제 버튼 클릭시 해당 데이터 삭제

--- src/views/admin-list/admin-list.js
+++ src/views/admin-list/admin-list.js
@@ -10,3 +10,44 @@ addAllElements();
 async function addAllElements() {
   header(headerParent);
 }
+
+// 삭제 버튼 다 들고오기
+const deleteBtn = document.querySelectorAll('.user-order-data td:last-child button');
+const tbody = $('.table tbody');
+
+for(let i = 0 ; i < deleteBtn.length; i ++){
+  deleteBtn[i].setAttribute('id',`deleteBtnId_${i+1}`);
+}
+let innerTrData = "";
+for(let i = 0 ; i < 10 ; i ++){
+  innerTrData += 
+  `<tr>
+  <td>admin</td>
+  <td>2022-06-0${i+1}</td>
+  <td>
+    <ul>
+      <li>남자옷</li>
+      <li>남자옷</li>
+      <li>남자옷</li>
+      <li>남자옷</li>
+    </ul>
+
+  </td>
+  <td>10개</td>
+  <td>배송중</td>
+  <td>
+    <button class="button is-danger is-light" onclick="deleteRow(this)">삭제</button>
+  </td>
+</tr>`
+}
+
+tbody.innerHTML = innerTrData;
+
+function deleteRow(o) {
+  const rowIndex = o.parentNode.parentNode.rowIndex;
+  document.querySelector('#mainTable').deleteRow(rowIndex);
+
+}
+
+
+  
