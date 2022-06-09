29f6c91d87efa7071a49bac1b9bb6d8d88c4f4c1
jeongmin SEO <all.the.lights.on.me@gmail.com>
2022-06-10 04:05:08 +0900

    Fix: html, css, js 수정 & Add footer 로고
    제곧내

--- src/views/product-list/product-list.js
+++ src/views/product-list/product-list.js
@@ -97,16 +97,3 @@ function scrollUp(e) {
   }
   
   scrollUp("scroll-btn");
-
-var aTags = document.querySelectorAll('header a');
-for(var i = 0; i < aTags.length; i ++) {
-    aTags[i].onclick = function(e) {
-        e.preventDefault();
-        var target = document.querySelector(this.getAttribute("href"));
-
-        window.scrollTo({
-            'behavior': 'smooth',
-            'top': target.offsetTop
-        })
-    }
-}
