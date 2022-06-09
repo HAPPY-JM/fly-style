fae3305ef49c40b83319ff38a8a33e535310825f
hb912 <43055448+hb912@users.noreply.github.com>
2022-06-10 01:15:41 +0900

    admin register 모달 구현

--- src/views/my-page/my-page.js
+++ src/views/my-page/my-page.js
@@ -10,21 +10,21 @@ landingRender();
 async function landingRender() {
   try {
     const userData = await Api.get(`/api/user`);
-    console.log(userData);
+    const userInfoEl = `
+    <div id="userName">
+        <h4 class="title is-4">${userData.fullName}</h4><p class="title is-6">님 안녕하세요!</p>
+    </div>
+
+    <div class="title is-5" id="userEmail">
+        <p>${userData.email}</p>
+    </div>
+  `;
+    userInfo.innerHTML = userInfoEl;
   } catch (e) {
     console.error(e.stack);
     alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${e.message}`);
     location.href = "/login";
   }
-  const userInfoEl = `
-              <div id="userName">
-                  <h4 class="title is-4">${userData.fullName}</h4><p class="title is-6">님 안녕하세요!</p>
-              </div>
 
-              <div class="title is-5" id="userEmail">
-                  <p>${userData.email}</p>
-              </div>
-            `;
-  userInfo.innerHTML = userInfoEl;
   header(headerParent);
 }
--- src/views/register/register.js
+++ src/views/register/register.js
@@ -1,13 +1,13 @@
-import * as Api from '/api.js';
+import * as Api from "/api.js";
 import header from "/header.js";
-import { validateEmail } from '/useful-functions.js';
+import { validateEmail } from "/useful-functions.js";
 
 // 요소(element), input 혹은 상수
-const fullNameInput = document.querySelector('#fullNameInput');
-const emailInput = document.querySelector('#emailInput');
-const passwordInput = document.querySelector('#passwordInput');
-const passwordConfirmInput = document.querySelector('#passwordConfirmInput');
-const submitButton = document.querySelector('#submitButton');
+const fullNameInput = document.querySelector("#fullNameInput");
+const emailInput = document.querySelector("#emailInput");
+const passwordInput = document.querySelector("#passwordInput");
+const passwordConfirmInput = document.querySelector("#passwordConfirmInput");
+const submitButton = document.querySelector("#submitButton");
 const headerParent = $("body");
 
 addAllElements();
@@ -15,76 +15,77 @@ addAllEvents();
 
 // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllElements() {
-	header(headerParent);
+  header(headerParent);
 }
-
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
-  submitButton.addEventListener('click', handleSubmit);
+  submitButton.addEventListener("click", handleSubmit);
 }
 
 // 회원가입 진행
 async function handleSubmit(e) {
-	e.preventDefault();
+  e.preventDefault();
 
-	const fullName = fullNameInput.value;
-	const email = emailInput.value;
-	const password = passwordInput.value;
-	const passwordConfirm = passwordConfirmInput.value;
+  const fullName = fullNameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  const passwordConfirm = passwordConfirmInput.value;
 
-	// 잘 입력했는지 확인
-	const isFullNameValid = fullName.length >= 2;
-	const isEmailValid = validateEmail(email);
-	const isPasswordValid = password.length >= 4;
-	const isPasswordSame = password === passwordConfirm;
+  // 잘 입력했는지 확인
+  const isFullNameValid = fullName.length >= 2;
+  const isEmailValid = validateEmail(email);
+  const isPasswordValid = password.length >= 4;
+  const isPasswordSame = password === passwordConfirm;
 
-	if (!isFullNameValid) {
-		return swal({
-			icon: "error",
-			text: '이름은 2글자 이상 입력해주세요.'
-		});
-	}
+  if (!isFullNameValid) {
+    return swal({
+      icon: "error",
+      text: "이름은 2글자 이상 입력해주세요.",
+    });
+  }
 
   if (!isPasswordValid) {
-		return swal({
-			icon: "error",
-			text: '비밀번호는 4글자 이상으로 설정해주세요.'
-		});
-	}
+    return swal({
+      icon: "error",
+      text: "비밀번호는 4글자 이상으로 설정해주세요.",
+    });
+  }
 
-	if (!isEmailValid) {
-		return swal({
-			icon: "error",
-			text: '이메일 형식이 맞지 않아요.'
-		});
-	}
+  if (!isEmailValid) {
+    return swal({
+      icon: "error",
+      text: "이메일 형식이 맞지 않아요.",
+    });
+  }
 
-	if (!isPasswordSame) {
-		return swal({
-			icon: "error",
-			text: '비밀번호가 일치하지 않아요.'
-		});
-	}
+  if (!isPasswordSame) {
+    return swal({
+      icon: "error",
+      text: "비밀번호가 일치하지 않아요.",
+    });
+  }
 
   // 회원가입 api 요청
   try {
     const data = { fullName, email, password };
 
-    await Api.post('/api/register', data);
+    await Api.post("/api/register", data);
 
     swal({
-			icon: "success",
-			text: '정상적으로 회원가입되었습니다.'
-		});
+      icon: "success",
+      text: "정상적으로 회원가입되었습니다.",
+      type: "success",
+    }).then(function () {
+      window.location.href = "/login";
+    });
 
     // 로그인 페이지 이동
-    window.location.href = '/login';
   } catch (err) {
     console.error(err.stack);
     alert({
-			icon: "error",
-			text: `문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`
-		});
+      icon: "error",
+      text: `문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`,
+    });
   }
 }
