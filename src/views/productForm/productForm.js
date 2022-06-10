b7a1f86fe8c8985b39f4b64b8213e457513aa537
GitSeongGit <102665966+GitSeongGit@users.noreply.github.com>
2022-06-10 12:08:09 +0900

    Update productForm.js

--- src/views/productForm/productForm.js
+++ src/views/productForm/productForm.js
@@ -13,17 +13,8 @@ const btn=document.getElementById("imgbutton");
 const inventorySubmit = document.getElementById("inventorySubmit")
 const outputdata = document.getElementById("outputdata")
 const sizeInput = document.getElementById("sizeInput")
-const sizeList = document.getElementsByClassName('sizeList')
-// function sizeListChange(a) {
-//     for(let i = 0; i < a.length; i++){
-//         const stock = []
-//         stock.push(a[i].innerText)
-//         return stock
-//     }
-    
-// } 
-// console.log(sizeListChange(sizeList.classlist))
-console.log(sizeList)
+const sizeList = document.querySelectorAll('sizeList')
+
 submitButton.addEventListener("click",submitEvent);
 imageInput.addEventListener('change', imageNameChange);
 inventorySubmit.addEventListener("click",sizeSubmitEvent)
@@ -31,8 +22,7 @@ console.log()
 function sizeSubmitEvent(e) {
     e.preventDefault();
     const size = [];
-    size.push(JSON.stringify({name :sizeInput.value , stock : Number(inventoryInput.value) }))
-    console.log(size)
+    const result1 = size.push(JSON.stringify({name :sizeInput.value , stock : Number(inventoryInput.value) }))
     const list =document.createElement("li");
     list.classList.add("sizeList");
     const newSize = document.createTextNode(size)
@@ -44,6 +34,7 @@ function sizeSubmitEvent(e) {
     //     console.log(values)
     //   }
     // console.log(...sizeList)
+    console.log(result1)
 }
 
 
@@ -56,17 +47,15 @@ function imageNameChange(e) {
 
 async function submitEvent(e) {
     e.preventDefault()
-
     const image =  imageInput.files[0];
     let formdata=new FormData();
-    // let sizeresult = []
     formdata.append('image-file',image);
     formdata.append('name',titleInput.value);
     formdata.append('price',priceInput.value);
     formdata.append('content',detailDescriptionInput.value);
     formdata.append('brand',manufacturerInput.value);
     formdata.append('category',categorySelectBox.value);
-    formdata.append('size',sizeList);
+    formdata.append('size',sizeList[0]);
     // formdata.append('',sizeInput.value)
     // formdata.append('',inventoryInput.value)
     // formdata.append('productQuantity',inventoryInput.value);
